7445768f83a45e98c947439603ae26238c285447
Carlos Barcos <fbarcos8@hotmail.com>
2020-11-30 00:02:40 -0500

    Sirviendo archivos estaticos
    	nuevo archivo:  public/home.html
    	nuevo archivo:  public/index.html
    	modificado:     server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
-  //res.send('Jilou guorld');
-  let salida = {
-    name: 'Carlos',
-    edad: 32,
-    url: req.url
-  }
-  res.send(salida);
-});
+app.use(express.static(__dirname + '/public'));
+
+// app.get('/',(req,res)=>{
+//   //res.send('Jilou guorld');
+//   let salida = {
+//     name: 'Carlos',
+//     edad: 32,
+//     url: req.url
+//   }
+//   res.send(salida);
+// });
 
 app.listen(3000,()=>{
   console.log('Escuchando peticiones en el puerto 3000');
