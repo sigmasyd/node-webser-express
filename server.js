bb4da30e410a7230d49b2df164cf87b383e82f3a
Carlos Barcos <fbarcos8@hotmail.com>
2020-11-30 11:42:16 -0500

    Agregando helpers
    	nuevo archivo:  hbs/helpers/helpers.js
    	modificado:     server.js
    	modificado:     views/home.hbs
    	modificado:     views/parciales/footer.hbs

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+require('./hbs/helpers/helpers');
 
 app.use(express.static(__dirname + '/public'));
 
+// Express HBS engine
 hbs.registerPartials(__dirname + '/views//parciales');
 app.set('view engine', 'hbs');
 
+// helpers
+
+
 app.get('/',(req,res)=>{  
   res.render('home.hbs',{
-    nombre: 'Sigma',
-    anio: new Date().getFullYear()
+    nombre: 'sigma syd'
   });
 });
 
 app.get('/about',(req,res)=>{  
-  res.render('about.hbs',{
-    nombre: 'Sigma',
-    anio: new Date().getFullYear()
-  });
+  res.render('about.hbs');
 });
 
 app.listen(3000,()=>{
