ded13446e5db7cd443af822037be2d58dbd59f9d
Carlos Barcos <fbarcos8@hotmail.com>
2020-11-29 23:17:29 -0500

    HTTP server basico 	nuevo archivo:  app.js 	nuevo archivo:  package.json

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+http
+  .createServer( (req,res) => {
+    res.writeHead(200,{'Content-Type':'application/json'});
+    let salida = {
+      name: 'Carlos',
+      edad: 32,
+      url: req.url
+    }
+    res.write(JSON.stringify(salida));
+    //res.write('Hola Mundo');
+    res.end();
+  })
+  .listen(8080)
+;
+console.log('Escuchando puerto 8080');
